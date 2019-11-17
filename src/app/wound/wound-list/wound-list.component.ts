5bc9872a9fb1f765c8ca62907a6240c7829d6238
Rotha <rotha.uy@gmail.com>
2019-11-16 23:37:45 -0600

    woundService is added

--- src/app/wound/wound-list/wound-list.component.ts
+++ src/app/wound/wound-list/wound-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Wound } from '../wound.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-wound-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wound-list.component.css']
 })
 export class WoundListComponent implements OnInit {
+  @Output() woundWasSelected = new EventEmitter<Wound>();
+
+  wounds: Wound[] = []
+  private woundsSub: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onWoundSelected(wound: Wound){
+    this.woundWasSelected.emit(wound);
   }
 
 }
--- src/app/wound/wound.service.ts
+++ src/app/wound/wound.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { Wound } from './wound.model';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({providedIn: 'root'})
@@ -8,5 +10,54 @@ export class WoundService {
     private wounds: Wound[] = [];
     private woundsUpdated = new Subject<Wound[]>();
 
+    constructor(private http: HttpClient) {}
 
+    getWounds(){
+        this.http
+            .get<{message: string, wounds: any}>("http://localhost:3000/api/wounds")
+            .pipe(map( (woundData) => {
+                return woundData.wounds.map(wound => {
+                    return {
+                        type: wound.type,
+                        description: wound.description,
+                        imagePath: wound.imagePath,
+                        id: wound._id,
+                        creator: wound.creator
+                    };
+                });
+            }))
+            .subscribe(transformedWounds => {
+                this.wounds = transformedWounds;
+                this.woundsUpdated.next([...this.wounds]);
+            });
+    }
+
+    getWoundUpdateListener() {
+        return this.woundsUpdated.asObservable();
+    }
+
+    getWound(type: string){
+        return {...this.wounds.find(p => p.type = type)};
+    }
+
+    addWound(type: string, desc: string, imagePath: string) {
+        const wound: Wound = {id: null, type: type, description: desc, imagePath: imagePath};
+        this.http
+            .post<{message: string, woundId: string}>("http://localhost:3000/api/wounds", wound)
+            .subscribe(responseData => {
+                const id = responseData.woundId;
+                wound.id = id;
+                this.wounds.push(wound);
+                this.woundsUpdated.next([...this.wounds]);
+            });
+    }
+
+    deleteWound(id: string) {
+        this.http.delete("http://localhost:3000/api/wounds" + id)
+            .subscribe(() => {
+                const updatedWounds = this.wounds.filter(wound => wound.id !== id);
+                this.wounds = updatedWounds;
+                this.woundsUpdated.next([...this.wounds]);
+            })
+    }
 }
